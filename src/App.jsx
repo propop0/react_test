bbfa10c184509dc6573e48f40412e0170d872f73
propop0 <bohdan.novachok@oa.edu.ua>
2025-09-21 18:40:36 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,16 @@ import './App.css'
 
 import ProductCard from './components/ProductCard';
 
-
-const products = [
-  {name : 'книга', price: 200, imageUrl: "https://via.placeholder.com/150/0000FF/FFFFFF?text=Book"},
-]
-
 function App() {
    const products = [
-    { id: 1, name: "Книга", price: 350, imageUrl: "https://via.placeholder.com/150/0000FF/FFFFFF?text=Book" },
+    { id: 1, name: "Книга", price: 350, imageUrl: "https://via.placeholder.com/150/0000FF/FFFFFF?text=Book"},
+    { id: 2, name: "Ноутбук", price: 15000, imageUrl: "https://via.placeholder.com/150/FF0000/FFFFFF?text=Laptop"},
   ];
   return (
      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
       {products.map(product => (
         <ProductCard
-          key={product.id} 
+          key={product.id}
           name={product.name}
           price={product.price}
           imageUrl={product.imageUrl}
