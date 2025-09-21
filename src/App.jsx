054395fc9a5d6df08c3efe5fdead869f8cb0592f
propop0 <bohdan.novachok@oa.edu.ua>
2025-09-21 19:05:56 +0300

    tarar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+// App.jsx
 import './App.css'
 
 import ProductCard from './components/ProductCard';
 
 function App() {
    const products = [
-    { id: 1, name: "Книга", price: 350, imageUrl: "https://via.placeholder.com/150/0000FF/FFFFFF?text=Book"},
-    { id: 2, name: "Ноутбук", price: 15000, imageUrl: "https://via.placeholder.com/150/FF0000/FFFFFF?text=Laptop"},
+    { id: 1, name: "Книга", price: 350, imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGigqPEkM_3r9UydjfJxEWnCuUfCHi3MGMHA&s"},
+    { id: 2, name: "Ноутбук", price: 15000, imageUrl: "https://lh6.googleusercontent.com/6w14yWXR5N9NkZyaT6mCwK0TGexXNFazvUztvrCEv7hM4JXWt6CsN2xeowBLON8k1Hkc6sT91mJDyYHVN_1okxLoMZ116i_qvmQl-IcYrEBwApB5pgvHd2pCqgXxmPL2FhWQ5JcL"},
   ];
   return (
-     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+    <div className="container"> 
       {products.map(product => (
         <ProductCard
           key={product.id}
